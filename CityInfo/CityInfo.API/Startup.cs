8627d4e83b4668a5cd3bb2b4b52830dca838dcc8
Matt Reeds <mattlreed@gmail.com>
2020-10-28 20:08:58 -0500

    refactored where connection string is stored

--- CityInfo/CityInfo.API/Startup.cs
+++ CityInfo/CityInfo.API/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Serialization;
 
@@ -15,6 +16,14 @@ namespace CityInfo.API
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration ?? 
+                throw new ArgumentNullException(nameof(configuration));
+
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -34,7 +43,7 @@ namespace CityInfo.API
 #else       
             services.AddTransient<IMailService, CloudMailService>();
 #endif
-            var connectionString = "Host=localhost;Port=5432;Database=CityInfoDB;User Id=postgres;Password=best_password_ever;";
+            var connectionString = _configuration["connectionStrings:cityInfoDBConnectionString"];
             services.AddDbContext<CityInfoContext>(o =>
             {
                 o.UseNpgsql(connectionString);
