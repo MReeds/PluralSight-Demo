405b5521dbcfcaeeb5aab546b372ec94adb91ae3
Matt Reeds <mattlreed@gmail.com>
2020-10-30 13:16:53 -0500

    created new migrations and seeded data

--- CityInfo/CityInfo.API/Migrations/20201030180337_SampleData.Designer.cs
+++ CityInfo/CityInfo.API/Migrations/20201030180337_SampleData.Designer.cs
@@ -2,15 +2,17 @@
 using CityInfo.API.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace CityInfo.API.Migrations
 {
     [DbContext(typeof(CityInfoContext))]
-    partial class CityInfoContextModelSnapshot : ModelSnapshot
+    [Migration("20201030180337_SampleData")]
+    partial class SampleData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
