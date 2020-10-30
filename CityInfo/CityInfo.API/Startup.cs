d9cf18667c8fee6c6f273ec78ad5b64b68b7c89b
Matt Reeds <mattlreed@gmail.com>
2020-10-30 13:58:52 -0500

    created interface and repository contract and registered in startup

--- CityInfo/CityInfo.API/Startup.cs
+++ CityInfo/CityInfo.API/Startup.cs
@@ -48,6 +48,8 @@ namespace CityInfo.API
             {
                 o.UseNpgsql(connectionString);
             });
+
+            services.AddScoped<ICityInfoRepository, CityInfoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
